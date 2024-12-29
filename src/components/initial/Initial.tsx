71368c08a64a143f2bd70d56baceaeb6950eed08
radek195 <radoslaw.parol95@gmail.com>
2024-12-29 20:49:08 +0100

    add nickname validation

--- src/components/initial/Initial.tsx
+++ src/components/initial/Initial.tsx
@@ -3,24 +3,39 @@ import {useState} from "react";
 
 export const Initial = ({setNickname}) => {
     const [inputValue, setInputValue] = useState('');
+    const [error, setError] = useState(true)
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
 
     const handleButtonClick = () => {
+        if (inputValue.length < 3) {
+            setError(true);
+            setTimeout(() => {
+                setError(false);
+            }, 2000)
+            return;
+        }
+
         setNickname(inputValue);
     };
+
+    const onFocus = () => {
+        setError(false);
+    }
 
     return (
         <div className="initial">
             <input
                 type="text"
-                className="input"
+                className={!error ? "input" : "input invalid"}
                 value={inputValue}
                 onChange={handleInputChange}
+                onFocus={onFocus}
                 placeholder="Nickname"
             />
+            {error && <p className="error">Nickname too short!</p>}
             <button
                 className="button"
                 onClick={handleButtonClick}
