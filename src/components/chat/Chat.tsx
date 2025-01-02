e65e4c8476d307c397cf7523407d04920da87ece
radek195 <radoslaw.parol95@gmail.com>
2025-01-02 22:56:39 +0100

    handle sending message with enter

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -20,8 +20,6 @@ export const Chat = ({myNickname}) => {
     }, [messages]);
 
     const renderMessages = messages.map(message => {
-
-
         return (
             myNickname == message.nickname ?
                 <p className={"message self"}
@@ -32,11 +30,11 @@ export const Chat = ({myNickname}) => {
     })
 
     const handleInputChange = (e) => {
-        console.log(text)
         setText(e.target.value);
     }
 
-    const handleSend = () => {
+    const handleSend = (e) => {
+        e.preventDefault()
         const newMessage: Message = {
             nickname: myNickname,
             content: text
@@ -44,6 +42,18 @@ export const Chat = ({myNickname}) => {
         setMessages([...messages, newMessage]);
         setText("");
     }
+
+    const handleEnterSend = (event) => {
+        if (event.key === "Enter" && !event.shiftKey) {
+            event.preventDefault(); // Prevent the default newline behavior in the textarea
+            const newMessage: Message = {
+                nickname: myNickname,
+                content: text
+            }
+            setMessages([...messages, newMessage]);
+            setText("");
+        }
+    };
 
     return (
         <>
@@ -56,16 +66,17 @@ export const Chat = ({myNickname}) => {
                 </div>
                 <div className={"controls"}>
                     <button>Go next!</button>
-                    <div className={"textbox"}>
-                        <textarea onChange={handleInputChange} value={text}/>
-                        <button onClick={handleSend}>
+                    <form className={"textbox"} onSubmit={handleSend}>
+                        <textarea onChange={handleInputChange} value={text} onKeyDown={handleEnterSend}/>
+                        <button>
                             <svg xmlns="http://www.w3.org/2000/svg"
                                  viewBox="0 0 512 512">
-                            <path
+                                <path
                                     d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z"/>
                             </svg>
                         </button>
-                    </div>
+                        <input type="submit" hidden/>
+                    </form>
                 </div>
             </main>
         </>
