8f8b80b50473560a7d41e4bb69048906e1e30fca
radek195 <radoslaw.parol95@gmail.com>
2025-03-17 11:48:15 +0100

    add https

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = `http://${import.meta.env.VITE_API_BASE_URL}`;
+axios.defaults.baseURL = `https://${import.meta.env.VITE_API_BASE_URL}`;
 
 export const getChatroom = async () => {
     return await axios.get(`/api/chatroom`);
